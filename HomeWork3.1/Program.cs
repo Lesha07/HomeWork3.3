54db67ed3f9d0a67b32ee735f24228e5adc6558c
Lesha07 <simchuklesha@gmail.com>
2024-12-30 00:58:42 +0200

    2

--- HomeWork3.1/Program.cs
+++ HomeWork3.1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HomeWork3._1;
+
+public class Program
+{
+    static void Main()
+    {
+        int maxGrade = 12;
+        int minGrade = 1;
+
+        var grades = new List<Student>
+        {
+            new Student
+            {
+                Name = "Alex",
+                Surname = "Florov",
+                Grade = 12,
+            },
+            new Student
+            {
+                Name = "Misha",
+                Surname = "Deminov",
+                Grade = 1,
+            },
+            new Student
+            {
+                Name = "Jack",
+                Surname = "Lomoch",
+                Grade = 7,
+            }
+        };
+
+        //int bestGrade = grades.Max(g => g.Grade);
+        var maxGrades = grades.OrderByDescending(x => x.Grade).First();
+        Console.WriteLine($"{maxGrades.Name} {maxGrades.Surname} grade: {maxGrades.Grade}");
+    }
+}
--- HomeWork3.1/Student.cs
+++ HomeWork3.1/Student.cs
@@ -2,5 +2,8 @@ namespace HomeWork3._1;
 
 public class Student
 {
-    
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Grade { get; set; }
 }
+
